ee75978a1e7644bf49aea80e1083a92219e32205
DevasenaInupakutika <devasena.prasad@gmail.com>
2016-05-19 14:40:55 +0100

    Updated typos clientId and secret and included ssl certifcate issue solution.

--- wooidc_server.js
+++ wooidc_server.js
@@ -7,7 +7,7 @@ OAuth.registerService('wooidc', 2, null, function (query) {
 
     var response = getTokens(query);
     var expiresAt = (+new Date) + (1000 * parseInt(response.expires_in, 10));
-    var accessToken = response.access_token;
+    var accessToken = response.data.access_token;
     var identity = getUserProfile(accessToken);
 
     var username = identity.name || identity.email;
@@ -48,30 +48,52 @@ if (Meteor.release) {
 }
 
 var getTokens = function (query) {
-    var config = getConfiguration();
-    var response =  {rejectUnauthorized: false};
+    var config = ServiceConfiguration.configurations.findOne({service: 'wooidc'});
+    if (!config)
+      throw new ServiceConfiguration.ConfigError();
+    
+    //getConfiguration();
+    var response;
+    console.log(config);
+    console.log(config.domain);
+    console.log(config.clientId);
+    console.log(OAuth.openSecret(config.clientSecret));
+    console.log(Meteor.absoluteUrl('_oauth/wooidc?close'));
+
     try {
 
-        response = HTTP.post(
-            'https://'+config.domain+'/oauth/token', {
+        var options = _.extend(Wooidc.httpOptions(), {
                 headers: {
                     Accept: 'application/json',
                     'User-Agent': userAgent
                 },
-                params: {
+               params: {
                     code:           query.code,
                     state:          query.state,
                     client_id:      config.clientId,
-                    client_secret:  OAuth.openSecret(config.secret),
+                    client_secret:  config.clientSecret,
                     grant_type:     'authorization_code',
-                    redirect_uri:   OAuth._redirectUri('wooidc',config)
-                    //Meteor.absoluteUrl('_oauth/wooidc?close')
+                    redirect_uri:   Meteor.absoluteUrl('_oauth/wooidc?close')
                 }
-            });
-            console.log(response);
+       });
+         
+        response = HTTP.post(
+            'https://' + config.domain +'/oauth/token', options 
+            );        
+
+        if(response.error) // if the http response was an error
+        {
+            throw response.error;
+        }
+        if(typeof response.content === 'string') {
+            response.content = JSON.parse(response.content);
+        }
+        if(response.content.error) {
+            throw response.content;
+        }
     }
 
-    catch (err) {
+    catch (err) {        
         throw _.extend(new Error('Failed to complete OAuth handshake with WO OIDCP. ' + err.message),
                       { response: err.response });
     }
@@ -119,6 +141,14 @@ Wooidc.retrieveCredential = function(credentialToken, credentialSecret) {
     var cred = OAuth.retrieveCredential(credentialToken, credentialSecret);
     console.log(cred);
     return cred;
+};
+
+Wooidc.httpOptions = function() {
+    if(Meteor.settings.wp_oauth_accept_invalid_certs) {
+        return { npmRequestOptions: { rejectUnauthorized: false } };
+    }
+    return {};
+
 };
 
 Meteor.publish('wooidcSessionState', function(){
