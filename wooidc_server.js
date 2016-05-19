0818cf064f391b07af355d15ee4876324a3ebb6a
DevasenaInupakutika <devasena.prasad@gmail.com>
2016-05-19 15:19:39 +0100

    syntax unexpected token error fix

--- wooidc_server.js
+++ wooidc_server.js
@@ -59,7 +59,7 @@ var getTokens = function (query) {
     console.log(config.domain);
     console.log(config.clientId);
     console.log(OAuth.openSecret(config.clientSecret));
-    console.log(OAuth._redirectUri('Wooidc',config);
+    console.log(OAuth._redirectUri('Wooidc',config));
 
     try {
 
