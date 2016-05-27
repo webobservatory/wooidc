c7ec59536b4a4aa25b18f49abf833ce54e9f121a
DevasenaInupakutika <devasena.prasad@gmail.com>
2016-05-27 10:03:26 +0100

    Minor updates to package.js header

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'devasena:wooidc',
-  version: '0.0.10',
+  name: 'webobservatory:wooidc',
+  version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'Southampton web observatory OpenID Connect Login flow',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/Southampton-RSG/wooidc.git',
+  git: 'https://github.com/webobservatory/wooidc.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -33,6 +33,6 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
-  api.use('devasena:wooidc');
+  api.use('webobservatory:wooidc');
   api.addFiles('wooidc-tests.js');
 });
